73eb50be558bfdb1049e74e4805f0efa503febb0
Robert Kłodziński <robert.klodzinski@gmail.com>
2018-10-10 23:27:11 +0200

    including config in the package

--- config/laravel.php
+++ config/laravel.php
@@ -7,7 +7,7 @@
  */
 
 return [
-    'wsdl'              => env('DRAGONPAY_WSDL1'),
-    'merchant_id'       => env('DRAGONPAY_MERCHANT_ID1'),
-    'merchant_password' => env('DRAGONPAY_MERCHANT_PASSWORD1'),
+    'wsdl'              => env('DRAGONPAY_WSDL'),
+    'merchant_id'       => env('DRAGONPAY_MERCHANT_ID'),
+    'merchant_password' => env('DRAGONPAY_MERCHANT_PASSWORD'),
 ];
