58f2f5247edecfbf68abc1118892e88a41363182
Robert Kłodziński <robert.klodzinski@gmail.com>
2018-10-12 21:59:23 +0200

    github #13: adding php doc

--- src/DragonPay/NormalizedEmail.php
+++ src/DragonPay/NormalizedEmail.php
@@ -10,6 +10,14 @@ namespace TendoPay\Integration\DragonPay;
 
 trait NormalizedEmail
 {
+    /**
+     * Normalizes the email by trimming it (removing spaces from the beginning and end of the string) and making it
+     * lowercase.
+     *
+     * @param string $email email to be normalized
+     *
+     * @return string normalized email
+     */
     public function normalizeEmail($email)
     {
         return trim(strtolower($email));
